1c01a9e0365b210314726ed146c4596162dafb07
Ioana Grosu <grosuioanaa@gmail.com>
2023-05-08 23:10:43 +0300

    Added signup functionality

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 require("./config/db");
 
 const express = require("express");
-const bodyParser = express.json;
 const cors = require("cors");
+const bodyParser = express.json;
+const routes = require("./routes")
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser());
+app.use("/api/v1", routes);
 
 module.exports = app;
