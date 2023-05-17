746bd5931720b51eceecaacde679515683df121d
Ioana Grosu <grosuioanaa@gmail.com>
2023-05-17 12:27:43 +0300

    Added get report by id method

--- src/domains/report/routes.js
+++ src/domains/report/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { createNewReport } = require("./controller");
 const router = express.Router();
+const Report = require("./model");
 
 router.post("/", async (req, res) => {
     try {
@@ -16,6 +17,23 @@ router.post("/", async (req, res) => {
         res.status(200).json(newReport);
     } catch (error) {
         res.status(400).send(error.message);
+    }
+});
+
+router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const report = await Report.findById(id); // Find the report by its ID
+
+        if (!report) {
+            return res.status(404).json({ error: "Report not found" });
+        }
+
+        return res.json(report);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Internal server error" });
     }
 });
 
