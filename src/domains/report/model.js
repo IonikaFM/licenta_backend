d13ff174e6a0d6503b9cbb690f44fc2581d27236
Ioana Grosu <igrosu@bitdefender.com>
2023-05-19 22:59:27 +0300

    Added type found/lost to report schema to diferentiate the reports

--- src/domains/report/model.js
+++ src/domains/report/model.js
@@ -2,18 +2,23 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ReportDetailsSchema = new Schema({
-    size: String,
-    colors: [String],
-    injured: Boolean,
-    agressive: Boolean,
+  size: String,
+  colors: [String],
+  injured: Boolean,
+  agressive: Boolean,
 });
 
 const ReportSchema = new Schema({
-    image: String,
-    details: ReportDetailsSchema,
-    latitude: Number,
-    longitude: Number,
-    date: Date,
+  type: {
+    type: String,
+    enum: ["FOUND", "LOST"],
+    required: true,
+  },
+  image: String,
+  details: ReportDetailsSchema,
+  latitude: Number,
+  longitude: Number,
+  date: Date,
 });
 
 const Report = mongoose.model("Report", ReportSchema);
