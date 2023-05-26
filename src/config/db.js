f129f36f1f8454263a7861802dcfcadb4cb8419a
Ioana Grosu <igrosu@bitdefender.com>
2023-05-26 14:12:09 +0300

    Modified structure

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,17 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const { WS_PORT } = process.env;
 const http = require("http");
-const { initializeWebSocket, emitEvent } = require("./websocket");
+const { Server } = require("socket.io");
 const app = require("../app");
 
 const { MONGODB_URI } = process.env;
 
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: `https://backend-licenta.onrender.com:${process.env.PORT}`,
+    },
+});
 const connectToDB = async () => {
     try {
         await mongoose.connect(MONGODB_URI, {
@@ -15,46 +20,9 @@ const connectToDB = async () => {
         });
 
         console.log("DB Connected");
-
-        setUpChangeStream(mongoose);
     } catch (error) {
         console.log(error);
     }
 };
 
-async function setUpChangeStream(mongoose) {
-    try {
-        // Get the 'reports' collection model
-        const Report = mongoose.model("Report");
-
-        // Set up the change stream
-        const changeStream = Report.watch();
-
-        // Start listening for changes
-        changeStream.on("change", (change) => {
-            // Handle the change event here
-            console.log("Change event:", change);
-            emitEvent(change);
-        });
-    } catch (error) {
-        console.error("Error setting up change stream:", error);
-    }
-}
-
-function startServerListener() {
-    try {
-        const server = http.createServer(app);
-
-        initializeWebSocket(server);
-
-        const port = WS_PORT;
-
-        server.listen(port, () => {
-            console.log("Server backend running on port " + port);
-        });
-    } catch (error) {
-        console.log("An exception occurred:", error);
-    }
-}
-
-module.exports = { startServerListener, connectToDB };
+module.exports = { connectToDB, server, io };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
-const app = require("./app");
-const { startServerListener, connectToDB } = require("./config/db");
+const { server, connectToDB, io } = require("./config/db");
 const { PORT } = process.env;
+const mongoose = require("mongoose");
 
 const startApp = () => {
     const port = PORT || 8080;
-    app.listen(port, () => {
+    server.listen(port, () => {
         console.log("Auth backend running on port " + port);
     });
-    connectToDB()
-        .then(startServerListener())
-        .catch((error) => console.log(error));
+    connectToDB().catch((error) => console.log(error));
+
+    io.on("connection", (socket) => {
+        console.log("Socket connected:", socket.id);
+
+        socket.on("disconnect", () => {
+            console.log("Socket disconnected:", socket.id);
+        });
+    });
+
+    try {
+        const Report = mongoose.model("Report");
+        const changeStream = Report.watch();
+
+        changeStream.on("change", (change) => {
+            console.log("Change event:", change);
+
+            if (change.operationType === "insert") {
+                io.emit("insert", change);
+            }
+        });
+    } catch (error) {
+        console.error("Error setting up change stream:", error);
+    }
 };
 
 startApp();
