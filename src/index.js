c9def688aba6b5288770eb2fa839b77fed4f3e3e
Ioana Grosu <igrosu@bitdefender.com>
2023-05-31 19:23:14 +0300

    Added insertCommit socket emit

--- src/index.js
+++ src/index.js
@@ -3,34 +3,49 @@ const { PORT } = process.env;
 const mongoose = require("mongoose");
 
 const startApp = () => {
-    const port = PORT || 8080;
-    server.listen(port, () => {
-        console.log("Auth backend running on port " + port);
-    });
-    connectToDB().catch((error) => console.log(error));
+	const port = PORT || 8080;
+	server.listen(port, () => {
+		console.log("Auth backend running on port " + port);
+	});
+	connectToDB().catch((error) => console.log(error));
 
-    io.on("connection", (socket) => {
-        console.log("Socket connected:", socket.id);
+	io.on("connection", (socket) => {
+		console.log("Socket connected:", socket.id);
 
-        socket.on("disconnect", () => {
-            console.log("Socket disconnected:", socket.id);
-        });
-    });
+		socket.on("disconnect", () => {
+			console.log("Socket disconnected:", socket.id);
+		});
+	});
 
-    try {
-        const Report = mongoose.model("Report");
-        const changeStream = Report.watch();
+	try {
+		const Report = mongoose.model("Report");
+		const changeStream = Report.watch();
 
-        changeStream.on("change", (change) => {
-            console.log("Change event:", change);
+		changeStream.on("change", (change) => {
+			console.log("Change event:", change);
 
-            if (change.operationType === "insert") {
-                io.emit("insert", change);
-            }
-        });
-    } catch (error) {
-        console.error("Error setting up change stream:", error);
-    }
+			if (change.operationType === "insert") {
+				io.emit("insertReport", change);
+			}
+		});
+	} catch (error) {
+		console.error("Error setting up change stream:", error);
+	}
+
+	try {
+		const Comment = mongoose.model("Comment");
+		const changeStream = Comment.watch();
+
+		changeStream.on("change", (change) => {
+			console.log("Change event:", change);
+
+			if (change.operationType === "insert") {
+				io.emit("insertComment", change);
+			}
+		});
+	} catch (error) {
+		console.error("Error setting up change stream:", error);
+	}
 };
 
 startApp();
