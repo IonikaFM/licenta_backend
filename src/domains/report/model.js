91fd29da47c91f65781fddbc819fa625519cb46d
Ioana Grosu <igrosu@bitdefender.com>
2023-06-02 20:29:23 +0300

    Added comments section for lost reports

--- src/domains/report/model.js
+++ src/domains/report/model.js
@@ -2,23 +2,24 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ReportDetailsSchema = new Schema({
-    size: String,
-    colors: [String],
-    injured: Boolean,
-    agressive: Boolean,
+	size: String,
+	colors: [String],
+	injured: Boolean,
+	agressive: Boolean,
+	comments: String,
 });
 
 const ReportSchema = new Schema({
-    type: {
-        type: String,
-        enum: ["FOUND", "LOST"],
-        required: true,
-    },
-    image: String,
-    details: ReportDetailsSchema,
-    latitude: Number,
-    longitude: Number,
-    date: Date,
+	type: {
+		type: String,
+		enum: ["FOUND", "LOST"],
+		required: true,
+	},
+	image: String,
+	details: ReportDetailsSchema,
+	latitude: Number,
+	longitude: Number,
+	date: Date,
 });
 
 const Report = mongoose.model("Report", ReportSchema);
